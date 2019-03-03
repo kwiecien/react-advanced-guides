292b6caccae9f1639e68fc9d9f7e1b31fa16160f
kwiecien <k.d.kwiecinski@gmail.com>
2019-03-03 16:26:11 +0100

    a11y: accessible forms

---
+++ src/accessibility/accessible-forms.tsx
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+
+function labeling() {
+    return (
+        <>
+            <label htmlFor="namedInput">Name:</label>
+            <input id="namedInput" type="text" name="name"/>
+        </>
+    )
+}
