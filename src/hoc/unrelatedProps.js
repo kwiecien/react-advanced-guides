5839f4544b5ff905398549ed90a5aa87c187bb5e
kwiecien <k.d.kwiecinski@gmail.com>
2019-03-04 21:15:52 +0100

    hoc: pass unrelated props through to the wrapped component

---
+++ src/hoc/unrelatedProps.js
@@ -0,0 +1,17 @@
+render() {
+    // Filter out extra props that are specific to this HOC and shouldn't be
+    // passed through
+    const { extraProp, ...passThroughProps } = this.props;
+
+    // Inject props into the wrapped component. These are usually state values or
+    // instance methods.
+    const injectedProp = someStateOrInstanceMethod;
+
+    // Pass props to wrapped component
+    return (
+        <WrappedComponent
+            injectedProp={injectedProp}
+            {...passThroughProps}
+        />
+    );
+}
